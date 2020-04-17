d175c09ee85c6d90443b42b5e5b1fbe558cbd173
Michele_Manco <motlamik@gmail.com>
2020-04-17 21:22:17 +0200

    working on lander

--- src/components/Lander.vue
+++ src/components/Lander.vue
@@ -23,20 +23,17 @@
                                 <div class="card-head">
                                     <img alt="Vue logo"
                                       class="rounded-circle img-custome"
-            src="https://www.wetechnology.cz/wp-content/uploads/2017/02/uxui.jpg">
-                                    <h2>web project</h2>
+                                      src="https://www.digitalocean.com/assets/media/logo-a721c4a7.png">
+                                    <h2>Digital Ocean </h2>
+                                    <span>Homepage mock-up</span>
                                 </div>
                                 <hr>
                                 <div class="card-body">
                                     <p>
-                                        Donec sed odio dui.
-                                  Etiam porta sem malesuada magna mollis euismod.
-                          Nullam id dolor id nibh ultricies vehicula ut id elit.
-                                Morbi leo risus, porta ac consectetur ac,
-                            vestibulum at eros. Praesent commodo cursus magna.
+                                        Uno dei primi progetti realizzatidurante il corso da Jr full stack web designer presso Boolean Careers è stato la realizazzione di una pagina mock-up della home-page di Digital Ocean utilizzando Html e Css ma implementando il Framework Bootstrap.
                                     </p>
                           <button type="button" class="btn btn-outline-secondary">
-                            More
+                            <a href="https://michele-manco.github.io/digitalocean/">More</a>
                             </button>
                                 </div>
                             </div>
@@ -45,17 +42,18 @@
                     <div class="col-md-4">
                             <div class="card">
                                 <div class="card-head">
-                                    <img alt="Vue logo" class="rounded-circle img-custome" src="https://i.pinimg.com/originals/4a/70/56/4a70563234301578c158dc142384a1c3.png">
-                                    <h2>web project</h2>
+                                    <img alt="Vue logo" class="rounded-circle img-custome" src="https://i.pinimg.com/originals/d0/f2/1f/d0f21fa5b2d5b08f61b3ccf9f9558ef4.png">
+                                    <h2>Spotify web</h2>
+                                    <span>web player</span>
                                 </div>
                                 <hr>
                                 <div class="card-body">
                                     <p>
-                                        Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.
-                                        Nullam id dolor id nibh ultricies vehicula ut id elit.
-                                        Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.
+                                        In questo esercizio, realizzato ad appena un mese dall' inizio del corso Boolean, abbiamo cercato di restituire un mock-up del lettore web della celebre audio app "Spotify", sempre con Html e Css ma utilizzando per la prima volta Flex.
                                     </p>
-                                    <button type="button" class="btn btn-outline-secondary">More</button>
+                                    <button type="button" class="btn btn-outline-secondary">
+                                      <a href="https://michele-manco.github.io/html-css-spotifyweb/">More</a>
+                                      </button>
                                 </div>
                             </div>
                         <br>
@@ -63,17 +61,18 @@
                     <div class="col-md-4">
                         <div class="card">
                             <div class="card-head">
-                                <img alt="Vue logo" class="rounded-circle img-custome" src="https://cdn.mos.cms.futurecdn.net/wH5y2PkWhCxqKBje8rBp5e.jpg">
-                                <h2>web project</h2>
+                                <img alt="Vue logo" class="rounded-circle img-custome" src="https://www.tuttotech.net/wp-content/uploads/2018/05/nuova-now-tv.png">
+                                <h2>Now tv</h2>
+                                <span>homepage mock-up</span>
                             </div>
                             <hr>
                             <div class="card-body">
                                 <p>
-                                    Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.
-                                    Nullam id dolor id nibh ultricies vehicula ut id elit.
-                                    Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.
+                                    Questa esercitazione rappresentava un pò una sorta di recap per tutta la parte iniziale del corso Boolean, quindi sempre Html e Css, ma ci permetteva di sperimentare con la struttura della pagina, introducendo un carosello ancorchè statico.
                                 </p>
-                                <button type="button" class="btn btn-outline-secondary">More</button>
+                                <button type="button" class="btn btn-outline-secondary">
+                                  <a href="https://michele-manco.github.io/-html-css-nowtv/">More</a>
+                                  </button>
                             </div>
                         </div>
                     </div>
@@ -84,20 +83,18 @@
                                 <div class="card-head">
                                     <img alt="Vue logo"
                                       class="rounded-circle img-custome"
-            src="https://www.wetechnology.cz/wp-content/uploads/2017/02/uxui.jpg">
-                                    <h2>web project</h2>
+                                      src="https://media-exp1.licdn.com/dms/image/C560BAQGbyUYc4amQsA/company-logo_200_200/0?e=2159024400&v=beta&t=KuqCdHQzYs5hYgyqYj1sfJBxV9EUoXtFGmt053q5pSE">
+                                    <h2>Helbiz</h2>
+                                    <span>Homepage mock-up</span>
                                 </div>
                                 <hr>
                                 <div class="card-body">
                                     <p>
-                                        Donec sed odio dui.
-                                  Etiam porta sem malesuada magna mollis euismod.
-                          Nullam id dolor id nibh ultricies vehicula ut id elit.
-                                Morbi leo risus, porta ac consectetur ac,
-                            vestibulum at eros. Praesent commodo cursus magna.
+                                        Questa sulla homepage di Helbiz è stata una bellissima esercitazione in Html e Css.
+                                        Non abbiamo utilizzato Bootstrap o altri frame-work e ci siamo concentrati sull'uso dei container e sulle position che,in un caso almeno ha avuto coordinate negative.
                                     </p>
                           <button type="button" class="btn btn-outline-secondary">
-                            More
+                            <a href="https://michele-manco.github.io/html-css-helbiz/">More</a>
                             </button>
                                 </div>
                             </div>
@@ -106,17 +103,18 @@
                     <div class="col-md-4">
                             <div class="card">
                                 <div class="card-head">
-                                    <img alt="Vue logo" class="rounded-circle img-custome" src="https://i.pinimg.com/originals/4a/70/56/4a70563234301578c158dc142384a1c3.png">
-                                    <h2>web project</h2>
+                                    <img alt="Vue logo" class="rounded-circle img-custome" src="https://yt3.ggpht.com/a/AATXAJwPQjfxo6S5qeDw-MLccI2iynowsSC-Akshig=s900-c-k-c0xffffffff-no-rj-mo">
+                                    <h2>Bool'flix (Netflix clone)</h2>
+                                    <span>search page</span>
                                 </div>
                                 <hr>
                                 <div class="card-body">
                                     <p>
-                                        Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.
-                                        Nullam id dolor id nibh ultricies vehicula ut id elit.
-                                        Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.
+                                        Progetto incentrato sulla replicazione di una search page ispirata da web-app come Netflix.Il mock-up,realizzato con Handlebars,effettua chiamate Ajax al DataBase di Movie Database, restituendo risultati sui più svariati argomenti.
                                     </p>
-                                    <button type="button" class="btn btn-outline-secondary">More</button>
+                                    <button type="button" class="btn btn-outline-secondary">
+                                      <a href="https://michele-manco.github.io/ajax-ex-boolflix/">More</a>
+                                      </button>
                                 </div>
                             </div>
                         <br>
@@ -124,17 +122,81 @@
                     <div class="col-md-4">
                         <div class="card">
                             <div class="card-head">
-                                <img alt="Vue logo" class="rounded-circle img-custome" src="https://cdn.mos.cms.futurecdn.net/wH5y2PkWhCxqKBje8rBp5e.jpg">
-                                <h2>web project</h2>
+                                <img alt="Vue logo" class="rounded-circle img-custome" src="https://image.shutterstock.com/image-vector/initial-letter-jq-logotype-company-260nw-1017450979.jpg">
+                                <h2>Mik Online Guitar Cave</h2>
+                                <span>JS/Jq Carousel</span>
                             </div>
                             <hr>
                             <div class="card-body">
                                 <p>
-                                    Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.
-                                    Nullam id dolor id nibh ultricies vehicula ut id elit.
-                                    Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.
+                                    Uno Slider costruito in Jq con autoplay e direzionamento manuale inserito all'interno di un progetto in costruzione.
+                                    .
+                                    .
+                                    Il sito sarà costruito con un occhio rivolto al Material Design di Google e si avvarrà delle funzionalità del framework VueJs.
                                 </p>
-                                <button type="button" class="btn btn-outline-secondary">More</button>
+                                <button type="button" class="btn btn-outline-secondary">
+                                  <a href="https://michele-manco.github.io/js-jq-carousel/">More</a>
+                                  </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-head">
+                                    <img alt="Vue logo"
+                                      class="rounded-circle img-custome"
+                                      src="https://i.pinimg.com/280x280_RS/5b/0c/12/5b0c121426e0632497aa36b3275a16dd.jpg">
+                                    <h2>Hub Spot</h2>
+                                    <span>homepage mock-up</span>
+                                </div>
+                                <hr>
+                                <div class="card-body">
+                                    <p>
+                                        La prima esercitazione di replicazione di un mock-up completo è stata quella inerente la homepage di HubSpot.Dopo solo due settimane dall'inizio del corso Boolean è sicuramente stato il primo vero "assaggio" di quello che è il lavoro di un web dev.
+                                    </p>
+                                    <button type="button" class="btn btn-outline-secondary">
+                                      <a href="https://michele-manco.github.io/html-css-hubspot/">More</a>
+                                      </button>
+                                </div>
+                            </div>
+                            <br>
+                    </div>
+                    <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-head">
+                                    <img alt="Vue logo" class="rounded-circle img-custome" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTRSUXMKaTPoUinRVBgPNMEl7pFKDujRsDrR37r-RYMrLxRvWUT&usqp=CAU">
+                                    <h2>wazzup web</h2>
+                                    <span>(Boolzup) web app</span>
+                                </div>
+                                <hr>
+                                <div class="card-body">
+                                    <p>
+                                        Questa esercitazione su di una web app per la messagistica era particolarmente orientata alla comprensione e all'uso delle chiamate Ajax. Utilizzavamo inoltre JS e Jquery per le parti relative alla gestione della rubrica ed invio msg.
+                                    </p>
+                                    <button type="button" class="btn btn-outline-secondary">
+                                      <a href="https://michele-manco.github.io/js-html-css-boolzap/">More</a>
+                                      </button>
+                                </div>
+                            </div>
+                        <br>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-head">
+                                <img alt="Vue logo" class="rounded-circle img-custome" src="https://adairtechs.com/images/ajax.jpg">
+                                <h2>Pagina di mock-up</h2>
+                                <span>web page con Ajax call</span>
+                            </div>
+                            <hr>
+                            <div class="card-body">
+                                <p>
+                                    Creazione di una semplice pagina tipo Netflix dove, con una chiamata Ajax, il filtro seleziona le card da caricare mentre il template viene gestito da Handlebars. Lo stesso esercizio è stato in seguito replicato in Php sia in Ajax che con codifica Json
+                                </p>
+                                <button type="button" class="btn btn-outline-secondary">
+                                  <a href="https://michele-manco.github.io/js-jq-ajax-api-musica/">More</a>
+                                  </button>
                             </div>
                         </div>
                     </div>
@@ -189,5 +251,17 @@ export default {
     }
     .card{
       margin-top: 30px;
+    }
+    .btn{
+      border:1px solid turquoise;
+
+    }
+    .btn:hover{
+      background-color:#34345238;
+      cursor:pointer;
+    }
+    a{
+      text-decoration:none;
+      color:turquoise;
     }
 </style>
